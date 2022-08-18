bc791e7a3ad879d2f420e08ec5b67f420a3637da
Alexxx1709 <alejandropera78@gmail.com>
2022-08-18 11:16:39 -0500

    Se agrega mensaje ecommerce

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h1>Ecommerce Alejandro Pérez</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
