af3e9b3d6fc6f72a2c62bf9ebdaf97710888fbf6
Alexxx1709 <alejandropera78@gmail.com>
2022-08-22 14:29:28 -0500

    Actualizacion para el desafio menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Ecommerce Alejandro Pérez</h1>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+    </>
+      
   );
 }
 
